9a542a5c9e929ca8947b449440a56fac054b325e
dymafr <erwan1309@hotmail.com>
2022-06-20 17:58:35 +0200

    1 file modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
-import Compteur from './Compteur';
 
 function App() {
+  console.log('Rendu du composant App');
   const [count, setCount] = useState(0);
+  const [showCount, setShowCount] = useState(true);
 
   function handleClick() {
     setCount(count + 1);
+    setShowCount(!showCount);
   }
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center p-20">
       <button onClick={handleClick} className="mb-20">
-        +1
+        +1 {showCount && <span> - Valeur : {count}</span>}
       </button>
-      <Compteur count={count} />
     </div>
   );
 }
